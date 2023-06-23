9b0046a8b646f98590472306e9fe7b8e23e07b84
KurtSoriano16 <127549518+KurtSoriano16@users.noreply.github.com>
2023-06-23 14:45:24 +0800

    Update enable_acquisition_transformation_configs.sql

--- EDW/Databases/edw/Scripts/Deployment Scripts/enable_acquisition_transformation_configs.sql
+++ EDW/Databases/edw/Scripts/Deployment Scripts/enable_acquisition_transformation_configs.sql
@@ -1,14 +1,11 @@
 UPDATE edw.etl.acquisition_configuration
 SET active_ind = 1
-OUTPUT 'UPDATE' AS Change INTO @SummaryOfChanges
 WHERE source_system_nm IN ('BOW','DOMAIN','INVEST','WORKDAY','GIT','NTTA_EDW') 
 
 UPDATE edw.etl.transformation_configuration
 SET active_ind = 1
-OUTPUT 'UPDATE' AS Change INTO @SummaryOfChanges
 WHERE source_system_nm IN ('BOW','DOMAIN','INVEST','GIT')
 
 UPDATE edw.[etl].[dataset_configuration]
 SET active_ind = 0
-OUTPUT 'UPDATE' AS Change INTO @SummaryOfChanges
 WHERE pipeline_nm IN ('PL_INVESTMENT_REFRESH_PBI_DATASET','PL_INVESTMENT_REFRESH_SNAPSHOT_DATASET','PL_FINANCIAL_REFRESH_PBI_DATASET')
