78deb962658fa51c3b4b6d3b32f2fabd56f31dc4
KurtSoriano16 <127549518+KurtSoriano16@users.noreply.github.com>
2023-06-23 10:25:13 +0800

    Update disable_acquisition_transformation_configs.sql

--- EDW/Databases/edw/Scripts/Deployment Scripts/disable_acquisition_transformation_configs.sql
+++ EDW/Databases/edw/Scripts/Deployment Scripts/disable_acquisition_transformation_configs.sql
@@ -1,3 +1,14 @@
-UPDATE DeploymentScriptsTest
-SET EnabledFlag = 0
-WHERE ConfigurationName IN ('Configuration1','Configuration2')
+UPDATE edw_etl_control.etl.acquisition_configuration
+SET active_ind = 0
+OUTPUT 'UPDATE' AS Change INTO @SummaryOfChanges
+WHERE source_system_nm IN ('BOW','DOMAIN','INVEST','INVEST SCORE','WORKDAY','GIT','NTTA_EDW')
+
+UPDATE edw_etl_control.etl.transformation_configuration
+SET active_ind = 0
+OUTPUT 'UPDATE' AS Change INTO @SummaryOfChanges
+WHERE source_system_nm IN ('BOW','DOMAIN','INVEST','INVEST SCORE','GIT')
+
+UPDATE edw_etl_control.[etl].[dataset_configuration]
+SET active_ind = 0
+OUTPUT 'UPDATE' AS Change INTO @SummaryOfChanges
+WHERE pipeline_nm IN ('PL_INVESTMENT_REFRESH_PBI_DATASET','PL_INVESTMENT_REFRESH_SNAPSHOT_DATASET','PL_FINANCIAL_REFRESH_PBI_DATASET')
