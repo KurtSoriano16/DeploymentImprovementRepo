74dea2f85efcb5865065ffc6a3200ac323e0ed47
KurtSoriano16 <127549518+KurtSoriano16@users.noreply.github.com>
2023-06-23 10:26:09 +0800

    Update enable_acquisition_transformation_configs.sql

--- EDW/Databases/edw/Scripts/Deployment Scripts/enable_acquisition_transformation_configs.sql
+++ EDW/Databases/edw/Scripts/Deployment Scripts/enable_acquisition_transformation_configs.sql
@@ -1,3 +1,9 @@
-UPDATE DeploymentScriptsTest
-SET EnabledFlag = 1
-WHERE ConfigurationName IN ('Configuration1','Configuration2')
+UPDATE edw_etl_control.etl.acquisition_configuration
+SET active_ind = 1
+OUTPUT 'UPDATE' AS Change INTO @SummaryOfChanges
+WHERE source_system_nm IN ('BOW','DOMAIN','INVEST','WORKDAY','GIT','NTTA_EDW') 
+
+UPDATE edw_etl_control.etl.transformation_configuration
+SET active_ind = 1
+OUTPUT 'UPDATE' AS Change INTO @SummaryOfChanges
+WHERE source_system_nm IN ('BOW','DOMAIN','INVEST','GIT')
