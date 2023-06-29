3ab731eb2bb39b02f90c3aa4a610bcc771559a9d
KurtSoriano16 <127549518+KurtSoriano16@users.noreply.github.com>
2023-06-29 14:01:55 +0800

    Update enable_dataset_configuration.sql

--- EDW/Databases/edw/Scripts/Deployment Scripts/enable_dataset_configuration.sql
+++ EDW/Databases/edw/Scripts/Deployment Scripts/enable_dataset_configuration.sql
@@ -1,3 +1,3 @@
 UPDATE edw.[etl].[dataset_configuration]
-SET active_ind = 0
+SET active_ind = 1
 WHERE pipeline_nm IN ('PL_INVESTMENT_REFRESH_PBI_DATASET','PL_INVESTMENT_REFRESH_SNAPSHOT_DATASET','PL_FINANCIAL_REFRESH_PBI_DATASET')
